19031dddae98ca5bd6b910688fce286af32529f2
edwin fernando <edwin.torrado04@gmail.com>
2025-09-21 12:24:03 -0500

    microservicio de arka gestor-solicitudes

---
+++ arka-cotizador/src/main/java/com/skillnest/arkacotizador/ArkaCotizadorApplication.java
@@ -0,0 +1,13 @@
+package com.skillnest.arkacotizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaCotizadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkaCotizadorApplication.class, args);
+    }
+
+}
